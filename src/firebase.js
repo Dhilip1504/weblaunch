606b1abb02aa8b22ed1b741e90a889d861a6a767
Dhilip kumar <dhilip1504@gmail.com>
2020-10-28 14:49:07 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDqUolecDKU7tSM-jpI4noHdXWL6B_jw2I",
+    authDomain: "whatsapp-clone-2a559.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-2a559.firebaseio.com",
+    projectId: "whatsapp-clone-2a559",
+    storageBucket: "whatsapp-clone-2a559.appspot.com",
+    messagingSenderId: "743282085583",
+    appId: "1:743282085583:web:f05f0fa691dd03a2ef71ba",
+    measurementId: "G-L76TWE2LEK"
+  };
